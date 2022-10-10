fd73075d833fba1d29354005059ffe15acf552ca
yashagarwal-ai <29867809+yash276@users.noreply.github.com>
2022-10-10 19:35:10 +0530

    WIP: variables name change and removed ReadME

--- src/createFolds.py
+++ src/createFolds.py
@@ -14,8 +14,8 @@ if __name__=="__main__":
     # Stratified ensures equal distribution of all classes in each fold
     kf =  model_selection.StratifiedKFold(n_splits=5,shuffle=False,random_state=42)
 
-    for fold,(trainId,valId) in enumerate(kf.split(X=df,y=df.target.values)):
-        print(len(trainId),len(valId))
-        df.loc[valId,'kfold'] = fold
+    for fold,(train_idx,val_idx) in enumerate(kf.split(X=df,y=df.target.values)):
+        print(len(train_idx),len(val_idx))
+        df.loc[val_idx,'kfold'] = fold
 
     df.to_csv('input/trainFolds.csv',index=False)

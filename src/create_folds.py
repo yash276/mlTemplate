5ae14ec4a6259ee958c03934dcb95b964d4eda8e
yashagarwal-ai <29867809+yash276@users.noreply.github.com>
2022-11-17 13:18:40 +0530

    Completing Cross Validatoin for all the types of problems

--- src/create_folds.py
+++ src/create_folds.py
@@ -1,21 +1,16 @@
 
-from sklearn import model_selection
+from . import cross_validation
 import pandas as pd
 
 if __name__=="__main__":
     # read the train CSV
-    df = pd.read_csv("input/train.csv")
-    # adding a kfold column with value -1
-    df['kfold'] = -1
-    # Random Sample the data with df.sample
-    # Reset the indices and the drop the Index column 
-    df = df.sample(frac=1).reset_index(drop=True)
-    # Using Stratified K fold
-    # Stratified ensures equal distribution of all classes in each fold
-    kf =  model_selection.StratifiedKFold(n_splits=5)
-
-    for fold,(train_idx,val_idx) in enumerate(kf.split(X=df,y=df.target.values)):
-        print(len(train_idx),len(val_idx))
-        df.loc[val_idx,'kfold'] = fold
-
-    df.to_csv('input/train_folds.csv',index=False)
+    df = pd.read_csv("input/multilabel/train.csv")
+    cv = cross_validation.CrossValidation(df,
+                         target_cols=["attribute_ids"],
+                         problem_type="multilabel_classification",
+                         multilabel_delimiter=" ",
+                         shuffle=True)
+    df_split = cv.split()
+    print(df_split.head()) 
+    print(df_split.kfold.value_counts())
+    # df_split.to_csv('input/train_folds.csv',index=False)
---
+++ src/cross_validation.py
@@ -0,0 +1,117 @@
+from sklearn import model_selection
+
+class CrossValidation:
+    """
+    binary classification
+    multi class classification
+    multi lable classification
+    single column regression
+    multi column regression
+    holdout
+    """
+    def __init__(self, 
+                 df, 
+                 target_cols,
+                 shuffle, 
+                 problem_type="binary_classification",
+                 multilabel_delimiter=",",
+                 num_folds=5,
+                 random_state=42
+                 ):
+        """_summary_
+
+        Args:
+            df (_type_): _description_
+            target_cols (_type_): _description_
+            shuffle (_type_): For Time Series shuffle needs to be False. 
+                            For other Problems it needs to be True
+            problem_type (str, optional): _description_. Defaults to "binary_classification".
+            num_folds (int, optional): _description_. Defaults to 5.
+            random_state (int, optional): _description_. Defaults to 42.
+        """
+        self.dataframe = df
+        self.target_cols = target_cols
+        self.num_targets = len(self.target_cols)
+        self.problem_type = problem_type
+        self.multilabel_delimiter = multilabel_delimiter
+        self.num_folds = num_folds
+        self.shuffle = shuffle
+        self.random_state = random_state
+        
+        if self.shuffle is True:
+            self.dataframe = self.dataframe.sample(frac=1).reset_index(drop=True)
+        self.dataframe['kfold'] = -1
+    
+    def split(self):
+        """_summary_
+
+        Raises:
+            Exception: _description_
+            Exception: _description_
+            Exception: _description_
+            Exception: _description_
+            Exception: _description_
+
+        Returns:
+            _type_: _description_
+        """
+        if self.problem_type in ("binary_classification", "multiclass_classification"):
+            if self.num_targets > 1 :
+                raise Exception("Invalid number of target for this problem type")
+            target = self.target_cols[0]
+            unique_values = self.dataframe[target].nunique()
+            if unique_values == 1:
+                # single target value so no point in creating the model
+                raise Exception("Only one unique value found for Target")
+            elif unique_values > 1:
+                # Binary classification problem
+                # use stratified k-fold
+                
+                kf =  model_selection.StratifiedKFold(n_splits=self.num_folds,
+                                                      shuffle=False
+                                                      )
+                for fold,(train_idx,val_idx) in enumerate(kf.split(X=self.dataframe,y=self.dataframe[target].values)):
+                    print(len(train_idx),len(val_idx))
+                    self.dataframe.loc[val_idx,'kfold'] = fold
+            
+        elif self.problem_type in ("single_column_regression","multi_column_regression"):
+            if self.num_targets != 1 and self.problem_type == "single_column_regression" :
+                raise Exception("Invalid number of target for this problem type")
+            if self.num_targets < 1 and self.problem_type == "multi_column_regression" :
+                raise Exception("Invalid number of target for this problem type")
+            
+            kf = model_selection.KFold(n_splits=self.num_folds,
+                                        shuffle=False
+                                        )
+            for fold,(train_idx,val_idx) in enumerate(kf.split(X=self.dataframe)):
+                print(len(train_idx),len(val_idx))
+                self.dataframe.loc[val_idx,'kfold'] = fold
+
+        elif self.problem_type.startswith("holdout_"):
+            holdout_percentage = int(self.problem_type.split('_')[1])
+            num_holdout_samples = int(len(self.dataframe) * (holdout_percentage) / 100)
+            self.dataframe.loc[:len(self.dataframe) - num_holdout_samples,"kfold"] = 0
+            self.dataframe.loc[len(self.dataframe) - num_holdout_samples:,"kfold"] = 1
+        
+        elif self.problem_type == "multilabel_classification":
+            """
+            We assume that the dataframe contains only one target column 
+            with each class seperated my a comma delimiter.
+
+            Raises:
+                Exception: _description_
+
+            Returns:
+                _type_: _description_
+            """
+            if self.num_targets !=1 :
+                    raise Exception("Invalid number of target for this problem type")
+            target = self.dataframe[self.target_cols[0]].apply(lambda x : len(str(x).split(self.multilabel_delimiter)))
+            kf =  model_selection.StratifiedKFold(n_splits=self.num_folds)
+            for fold,(train_idx,val_idx) in enumerate(kf.split(X=self.dataframe,y=target)):
+                print(len(train_idx),len(val_idx))
+                self.dataframe.loc[val_idx,'kfold'] = fold
+        else:
+            raise Exception("Funnny problem type not Understood")
+            
+        return self.dataframe
